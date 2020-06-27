fcb5323f741a4673c623a2d92040d8c049205459
Denis Legezo <denis.legezo@kaspersky.com>
2020-06-27 13:30:48 +0300

    Origami Elephant C2 decryptor optimisation

--- base/base.cpp
+++ base/base.cpp
@@ -3,11 +3,19 @@
 using namespace std;
 
 // TODO add template to build from any container, not only strings
-// TODO consider refactor as class with copy constructor
-storage_p build_storage(const string &s) {
-	storage r;
-	for (auto e : s) {
-	  r.push_back(e);
-	}
-	return make_shared<storage>(r);
+storage_p build_storage_string(const string &val) {
+  storage r;
+  for (auto e : val) {
+    r.push_back(e);
+  }
+  return make_shared<storage>(r);
+}
+
+storage_p build_storage_bin(unsigned char *val, int len) {
+  storage r;
+  int i;
+  for (i=0; i<len; ++i) {
+      r.push_back(val[i]);
+    }
+  return make_shared<storage>(r);
 }
--- base/base.h
+++ base/base.h
@@ -3,9 +3,11 @@
 #include <vector>
 #include <iostream>
 #include <memory>
+#include <z3.h>
 
 typedef std::vector<uint8_t> storage;
-typedef std::shared_ptr<std::vector<uint8_t>> storage_p;
-typedef std::vector<uint8_t>::iterator storage_it;
+typedef std::shared_ptr<storage> storage_p;
+typedef storage::iterator storage_it;
 
-storage_p build_storage(const std::string &s);
+storage_p build_storage_string(const std::string &val);
+storage_p build_storage_bin(unsigned char *val, int len);
